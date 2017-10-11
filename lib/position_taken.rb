24aa4ca4b1cb72605010d8d1672199501555ebac
Leslie Hsu <chuckleberries@hotmail.com>
2017-10-11 20:14:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(input)
+  index_number = input.to_i
+  if array(index_number) == " " || array(index_number) == "" || array(index_number) == nil
+    return false
+  else
+    return true
+  end
+end
